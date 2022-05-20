931b321eb7c1110f532ffc6feee841579e0d2531
周顺顺 <idioticzhou@foxmail.com>
2022-05-20 13:50:18 +0800

    (vue-i18n): 配置国际化功能

---
+++ src/locales/langs/zh-CN.ts
@@ -0,0 +1,9 @@
+import { genMessage } from '..';
+
+const modules = import.meta.globEager('./zh-CN/**/*.ts');
+
+export default {
+  message: {
+    ...genMessage(modules, 'zh-CN'),
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { setupI18n } from './locales';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+async function setup() {
+  await setupI18n(app);
+}
+setup();
