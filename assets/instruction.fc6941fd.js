4862286e752e880b4a14e3f3d5156f56633d6968
Zhoushunshun541 <Zhoushunshun541@users.noreply.github.com>
2022-05-31 00:43:31 +0000

    deploy: 0817ceedf1b79acf1682a39b280d36965b1b6e6a

--- assets/instruction.fc6941fd.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./request.5d9bb19a.js";import{e as r,k as a,o as s,m as i,ap as c}from"./index.964b457b.js";const p=["innerHTML"],_=r({name:"instruction",setup(u){const n=a();function e(){n.intro||o.getIntro().then(t=>{n.saveIntro(t.data)})}return e(),(t,m)=>(s(),i("div",{innerHTML:c(n).intro},null,8,p))}});export{_ as default};
